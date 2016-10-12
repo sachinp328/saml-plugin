502f9336ae486c4e498d88f4d58b88aa6129561d
Scotty Logan <swl@stanford.edu>
2016-10-11 18:31:00 -0700

    added expirationTime to SamlAuthenticationToken

--- src/main/java/org/jenkinsci/plugins/saml/SamlAuthenticationToken.java
+++ src/main/java/org/jenkinsci/plugins/saml/SamlAuthenticationToken.java
@@ -17,22 +17,44 @@ under the License. */
 
 package org.jenkinsci.plugins.saml;
 
+import org.kohsuke.stapler.Stapler;
+
 import org.acegisecurity.providers.AbstractAuthenticationToken;
+import org.acegisecurity.context.SecurityContextHolder;
 
 public class SamlAuthenticationToken extends AbstractAuthenticationToken {
 
   private static final long serialVersionUID = 2L;
 
   private final SamlUserDetails userDetails;
+  private final long expirationTime;
 
-  public SamlAuthenticationToken(SamlUserDetails userDetails) {
+  public SamlAuthenticationToken(SamlUserDetails userDetails, long expirationTime) {
     super(userDetails.getAuthorities());
     this.userDetails = userDetails;
     this.setDetails(userDetails);
     this.setAuthenticated(true);
+    this.expirationTime = expirationTime;
+  }
+
+  public SamlAuthenticationToken(SamlUserDetails userDetails) {
+    this(userDetails, 0);
   }
 
   public SamlUserDetails getPrincipal() {
+    // check if session should have expired
+    if (expirationTime > 0 && System.currentTimeMillis() > expirationTime) {
+      // sometimes getCurrentRequest() returns null
+      if (Stapler.getCurrentRequest() != null) {
+        // terminate the current session
+        this.setAuthenticated(false);
+        if (Stapler.getCurrentRequest().getSession() != null) {
+          Stapler.getCurrentRequest().getSession().invalidate();
+        }
+        SecurityContextHolder.clearContext();
+      }
+    }
+
     return userDetails;
   }
 
